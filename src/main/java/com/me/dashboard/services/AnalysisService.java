0d453b264820a5a26ed057b2c7975661375cc3c2
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-06-17 19:45:08 +0530

    Add AnalysisService

---
+++ src/main/java/com/me/dashboard/services/AnalysisService.java
@@ -0,0 +1,12 @@
+package com.me.dashboard.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnalysisService {
+
+    public String analyse() {
+
+        return "Out";
+    }
+}
