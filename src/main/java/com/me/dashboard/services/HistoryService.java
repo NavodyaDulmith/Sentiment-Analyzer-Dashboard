756d35272a2fba73a7e8b4526f9a78b888a0da8b
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-06-23 21:09:52 +0530

    Add History Controller and Service

---
+++ src/main/java/com/me/dashboard/services/HistoryService.java
@@ -0,0 +1,12 @@
+package com.me.dashboard.services;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class HistoryService {
+
+    public String getHistory() {
+
+        return "Out";
+    }
+}
