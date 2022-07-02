64003486b625eab6ba9bc691c81b516e2103bedb
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-07-02 22:47:35 +0530

    Update sidebar

--- src/main/resources/dashboard/src/components/sidebar/SideBar.js
+++ src/main/resources/dashboard/src/components/sidebar/SideBar.js
@@ -1,17 +1,33 @@
 import React, {Component} from "react";
 import "../../css/SideBar.css";
 import {Link} from "react-router-dom";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faUser} from "@fortawesome/free-solid-svg-icons";
 
+const style = {
+  position: "absolute",
+  bottom: "40px",
+};
 class SideBar extends Component {
   render() {
     return (
       <div class="sidenav">
+        <div class="container nav-link text-center">
+          <FontAwesomeIcon icon={faUser} size="3x" />
+          <p class="">Admin</p>
+        </div>
         <Link to={""} className="nav-link">
-          Analyzer
+          Analayzer
         </Link>
         <Link to={"history"} className="nav-link">
-          History
+          Dashboard
         </Link>
+        <Link className="nav-link">History</Link>
+        <Link className="nav-link">Profile</Link>
+        <Link className="nav-link">About Us</Link>
+        <div style={style}>
+          <Link className="nav-link">Sign Out</Link>
+        </div>
       </div>
     );
   }
