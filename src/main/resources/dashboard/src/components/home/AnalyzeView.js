f959a86c250c97f3a339f5e0ba8ccb3f188fd051
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-07-03 12:51:00 +0530

    Update ui

--- src/main/resources/dashboard/src/components/home/AnalyzeView.js
+++ src/main/resources/dashboard/src/components/home/AnalyzeView.js
@@ -1,9 +1,6 @@
 import React, {Component} from "react";
 
-import {Button, Col, Form} from "react-bootstrap";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-/* import MyToast from "./MyToast";
- */
+import {Col, Form} from "react-bootstrap";
 import axios from "axios";
 import "../../css/Issue.css";
 
@@ -12,14 +9,13 @@ export default class AnalyzeView extends Component {
     super(props);
     this.state = this.initialState;
     this.state.show = false;
+    this.state.output = "";
     this.projectChange = this.projectChange.bind(this);
     this.submitProject = this.submitProject.bind(this);
   }
 
   initialState = {
     name: "",
-    key: "",
-    path: "",
   };
 
   resetProject = () => {
@@ -29,22 +25,19 @@ export default class AnalyzeView extends Component {
   submitProject = (event) => {
     event.preventDefault();
 
-    const project = {
+    const text = {
       name: this.state.name,
-      key: this.state.key,
-      path: this.state.path,
     };
 
-    alert("Violation Scanning Started!");
     axios
-      .post("http://localhost:8080/api/v1/cvf/addProject", project)
+      .post("http://localhost:8080/api/v1/csa/analyse", text)
       .then((response) => {
+        console.log(response);
         if (response.data != null) {
+          console.log(response.data);
           this.setState({ show: true });
-          setTimeout(() => this.setState({ show: false }), 3000);
-          alert("Project Successfully Added!");
-        } else {
-          this.setState({ show: false });
+          this.setState({ output: response.data });
+          console.log("set output");
         }
       });
 
@@ -58,16 +51,17 @@ export default class AnalyzeView extends Component {
   };
 
   render() {
-    const { name, key, path } = this.state;
+    const { name } = this.state;
 
     return (
       <div>
-        <div className="jumbotron bg-secondary text-white background">
-          <div id="left">
-            <h1 className="display-5">CoVacciMOOD Analyzer</h1>
+        <div class="jumbotron jumbotron-fluid jumbo_style jumbo-img">
+          <div class="container">
+            <h1 class=" font-weight-bold text-dark">
+              CoVacciMOOD Sentiment Analayzer
+            </h1>
           </div>
         </div>
-
         <Form
           onReset={this.resetProject}
           onSubmit={this.submitProject}
@@ -87,16 +81,35 @@ export default class AnalyzeView extends Component {
                 as="textarea"
               />
             </Form.Group>
-            <Button
-              class="btnsize"
-              size="lg"
-              variant="outline-success"
-              type="submit"
-            >
-              <FontAwesomeIcon icon={"cogs"} /> Analyze
-            </Button>
+            <button type="submit" class="btn btn-outline-success btn_size">
+              Analyze
+            </button>
           </Form.Row>
         </Form>
+        {this.state.show === true ? (
+          <div class="box">
+            <div class="container">
+              <h3 class="font-weight-light">Prediction Result</h3>
+              <hr></hr>
+              <p>Analyzed Text :Here's what I've done so far. {this.state.input} </p>
+              <hr></hr>
+              <p class="text-danger font-weight-bold">Classified Class :Positive</p>
+              <br></br>
+              <p class="">Positive Pobability :</p>
+              <p class="">Negative Pobability :</p>
+              <p class="">Neutral Pobability :</p>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+        {this.state.loading === true ? (
+          <div class="box">
+            <div class="container"></div>
+          </div>
+        ) : (
+          <div></div>
+        )}
       </div>
     );
   }
