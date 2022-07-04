f83a8f0b7a209dc88160d95c7542647990d04a45
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-07-04 22:45:37 +0530

    Add SentimentResultCrud

---
+++ src/main/java/com/me/dashboard/database/crud/SentimentResultCrud.java
@@ -0,0 +1,27 @@
+package com.me.dashboard.database.crud;
+
+import com.me.dashboard.database.entities.SentimentResult;
+import com.me.dashboard.database.repository.SentimentResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class SentimentResultCrud {
+
+    private SentimentResultRepository sentenceRepository;
+
+    @Autowired
+    public SentimentResultCrud(SentimentResultRepository repository) {
+        this.sentenceRepository = repository;
+    }
+
+    public void addAll(List<SentimentResult> list) {
+        sentenceRepository.saveAll(list);
+    }
+
+    public List<SentimentResult> getAll() {
+        return sentenceRepository.findAll();
+    }
+}
