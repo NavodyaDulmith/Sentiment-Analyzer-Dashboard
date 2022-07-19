c2e5b996648941a3af8ed729316b4d0c9e56a015
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-07-20 00:10:04 +0530

    Get data from prediction

--- src/main/java/com/me/dashboard/services/AnalysisService.java
+++ src/main/java/com/me/dashboard/services/AnalysisService.java
@@ -6,6 +6,7 @@ import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
+import com.me.dashboard.client.HttpClient;
 import com.me.dashboard.database.crud.SentimentResultCrud;
 import com.me.dashboard.database.entities.SentimentResult;
 import com.me.dashboard.dto.ResultDTO;
@@ -15,10 +16,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import java.util.Map;
 
 @Component
 public class AnalysisService {
@@ -26,16 +29,26 @@ public class AnalysisService {
     @Autowired
     SentimentResultCrud sentimentResultCrud;
 
+    @Autowired
+    HttpClient httpClient;
+
     private static final Logger logger = LoggerFactory.getLogger(AnalysisService.class);
 
-    public String analyse() {
-        //call py api
-        //get response
-        logger.info("Result Successfully Received!");
-        //save result
-        //addResult("payload", sentence);
+    public String analyse(String payload) throws IOException {
 
-        return "payload";
+        JsonObject obj = (JsonObject) new JsonParser().parse(payload);
+        String text = (obj.get("text").getAsString());
+
+        //call prediction api
+        Map<String, Object> res = httpClient.sendPost("https://covaccimood-service-57e4ykq7iq-uc.a.run.app/predict",payload);
+        res.put("text",text);
+        if (res.get("status").equals(200)){
+            addResult(String.valueOf(res.get("response")),text);
+            logger.info("Result Successfully Received!");
+            return String.valueOf(res.get("response"));
+        }
+
+        return String.valueOf(res.get("response"));
     }
 
     public String addResult(String payload, String sentence) {
@@ -43,16 +56,17 @@ public class AnalysisService {
         List<SentimentResult> list = new ArrayList<>();
         SentimentResult record = new SentimentResult();
         record.setSentence(sentence);
-        record.setConfidence(obj.get("").getAsLong());
-        record.setNegative(obj.get("").getAsLong());
-        record.setPositive(obj.get("").getAsLong());
-        record.setNeutral(obj.get("").getAsLong());
-        record.setSentiment(obj.get("").getAsString());
+        record.setNegative(obj.get("probabilities").getAsJsonObject().get("negative").getAsDouble());
+        record.setPositive(obj.get("probabilities").getAsJsonObject().get("positive").getAsDouble());
+        record.setNeutral(obj.get("probabilities").getAsJsonObject().get("neutral").getAsDouble());
+        record.setConfidence(obj.get("confidence").getAsDouble());
+        record.setSentiment(obj.get("sentiment").getAsString());
+        //add timestamp
         String timeStamp = new SimpleDateFormat("dd-M-yyyy hh:mm:ss").format(Calendar.getInstance().getTime());
         record.setCreationDate(timeStamp);
         list.add(record);
         sentimentResultCrud.addAll(list);
-        logger.info("Results Successfully Added!");
+        logger.info("Results Successfully Added to DB!");
         return record.getSentence();
     }
 
