3ae295a5900a61510f0b79defdbe3d753604a653
Navodya Dulmith <36164924+NavodyaDulmith@users.noreply.github.com>
2022-07-20 00:25:57 +0530

    Remove unused methods

--- src/main/java/com/me/dashboard/client/HttpClient.java
+++ src/main/java/com/me/dashboard/client/HttpClient.java
@@ -3,7 +3,6 @@ package com.me.dashboard.client;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
@@ -22,41 +21,6 @@ import java.util.Map;
 public class HttpClient {
 
     Logger logger = LoggerFactory.getLogger(HttpClient.class);
-
-    public Map sendGet(String url) throws IOException {
-
-        int counter = 0;
-        Map response = null;
-        while (counter < 10) {
-            response = trySendGET(url);
-            if ((int) response.get("status") != 401) {
-                return response;
-            }
-            counter++;
-        }
-        return response;
-
-    }
-
-    private Map<String, Object> trySendGET(String url) throws IOException, ClientProtocolException {
-
-        HttpGet request = new HttpGet(url);
-
-        try (CloseableHttpClient httpClient = HttpClients.createDefault();
-             CloseableHttpResponse response = httpClient.execute(request)) {
-            Map<String, Object> res = new HashMap();
-            HttpEntity entity = response.getEntity();
-
-            if (entity != null) {
-                logger.info("get url - > " + url);
-                logger.info("status - > " + response.getStatusLine().getStatusCode());
-                res.put("response", EntityUtils.toString(entity));
-                res.put("status", response.getStatusLine().getStatusCode());
-            }
-            return res;
-        }
-
-    }
 
     public Map<String, Object> sendPost(String url, String payload) throws ClientProtocolException, IOException {
 
